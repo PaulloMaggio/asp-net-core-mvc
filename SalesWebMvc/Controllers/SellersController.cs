0e4c9e1fc816ded372b012caecd23832ee86a4ba
Paulo Fernando Maggio <paullomagio@gmail.com>
2026-04-10 15:07:48 -0300

    Simple Create form && Foreign key not null (referential integrity) && SellerFormViewModel and Department select component

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
         private readonly Services.SellerService _sellerService;
+        private readonly Services.DepartmentService _departmentService;
 
-        public SellersController(Services.SellerService sellerService)
+        public SellersController(Services.SellerService sellerService, Services.DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -18,6 +21,19 @@ namespace SalesWebMvc.Controllers
             return View(list);
         }
 
-
+        public IActionResult Create()
+        {
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Models.Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
